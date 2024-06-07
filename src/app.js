1c5ba3a2abca1abe3e6ee644245d55d99d319149
logic lly <ellybachubila@gmail.com>
2024-06-07 20:40:00 +0300

    implement student registration serving db student data initialized prisma

--- src/app.js
+++ src/app.js
@@ -6,12 +6,59 @@ dotenv.config({
   path: ".env.development.local",
 });
 
+const port = process.env.PORT;
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const navlinks = {
   teachers: ["home", "students", "classes", "modules"],
   students: ["home", "teachers", "classes", "modules"],
   modules: ["home", "teachers", "students", "classes"],
   classes: ["home", "teachers", "students", "modules"],
 };
+
+const modules = [
+  {
+    name: "Entrepreneurship",
+    code: "GSU 08211",
+    credits: 6,
+    teacherId: "12012023045",
+  },
+  {
+    name: "Project",
+    code: "ETU 08221",
+    credits: 12,
+    teacherId: "12012023046",
+  },
+  {
+    name: "Radar",
+    code: "ETU 08222",
+    credits: 9,
+    teacherId: "12012023047",
+  },
+  {
+    name: "Broadcasting",
+    code: "ETU 08223",
+    credits: 9,
+    teacherId: "12012023048",
+  },
+  {
+    name: "Satellite",
+    code: "ETU 08224",
+    credits: 9,
+    teacherId: "12012023049",
+  },
+  {
+    name: "Robotics",
+    code: "COU 08202",
+    credits: 9,
+    teacherId: "12012023050",
+  },
+];
 
 const { Pool } = pkg;
 
@@ -23,6 +70,33 @@ const pool = new Pool({
   user: "postgres",
   connectionTimeoutMillis: 3500,
 });
+
+const insertStudentData = `INSERT INTO STUDENTS (registrationNumber, name) VALUES ($1, $2) RETURNING *`;
+
+const insertTeacherData = `INSERT INTO TEACHERS (id, name) VALUES ($1, $2)`;
+
+const insertModulesData = `INSERT INTO MODULES (code, name, credits, teacherId) VALUES ($1, $2, $3, $4)`;
+
+async function insertModules() {
+  try {
+    // Loop through the modules array
+    for (const _module of modules) {
+      const { code, name, credits, teacherId } = _module;
+
+      // Execute the query for each module
+      await pool.query(insertModulesData, [code, name, credits, teacherId]);
+    }
+
+    console.log("Modules inserted successfully!");
+    return { success: true };
+  } catch (err) {
+    console.error("Error inserting modules:", err);
+    return { success: false };
+  } finally {
+    // Release the client back to the pool
+    pool.end();
+  }
+}
 
 const createStudentsTable = `CREATE TABLE IF NOT EXISTS STUDENTS 
   (
@@ -66,12 +140,6 @@ const fetchModules = `SELECT * FROM modules`;
 
 const fetchStudents = `SELECT * FROM students`;
 
-const port = process.env.PORT;
-const app = express();
-
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-
 app.get("/", async (req, res) => {
   return res.render("pages/index", {
     title: "homepage",
@@ -83,9 +151,12 @@ app.get("/home", (req, res) => {
 });
 
 app.get("/students", async (req, res) => {
+  const students = await pool.query(fetchStudents);
+  console.log(students.rows);
   return res.render("pages/students", {
     title: "students page",
     navlinks: navlinks.students,
+    students: students.rows,
   });
 });
 
@@ -127,15 +198,6 @@ app.get("/classes/:name", async (req, res) => {
   });
 });
 
-let modules = [
-  { name: "Entrepreneurship", code: "GSU 08211", credits: 6, teacherId: "" },
-  { name: "Project", code: "ETU 08221", credits: 12, teacherId: "" },
-  { name: "Radar", code: "ETU 08222", credits: 9, teacherId: "" },
-  { name: "Broadcasting", code: "ETU 08223", credits: 9, teacherId: "" },
-  { name: "Satellite", code: "ETU 08224", credits: 9, teacherId: "" },
-  { name: "Robotics", code: "COU 08202", credits: 9, teacherId: "" },
-];
-
 app.get("/modules", async (req, res) => {
   return res.render("pages/modules", {
     title: "Modules page",
@@ -152,6 +214,21 @@ app.get("/modules/:code", async (req, res) => {
     _module,
     navlinks: navlinks.modules,
   });
+});
+
+app.post("/student-registration", async (req, res) => {
+  const { registrationNumber, name } = req.body;
+  try {
+    const student = await pool.query(insertStudentData, [
+      registrationNumber,
+      name,
+    ]);
+    console.log("Student inserted successfully!\n", student.rows);
+    return res.redirect("/students");
+  } catch (err) {
+    console.error("Error inserting student:", err);
+    return res.redirect("/new-student");
+  }
 });
 
 app.listen(port, () => {
