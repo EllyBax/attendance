4e9accc9d83a3bfe82913303a41d3d18a05e1b8a
Elly Bax <baxllycc@gmail.com>
2024-06-08 15:42:07 +0300

    On to attendance recording fetching and serving all db data needed

--- src/app.js
+++ src/app.js
@@ -21,44 +21,71 @@ const navlinks = {
   classes: ["home", "teachers", "students", "modules"],
 };
 
-const modules = [
+const teachers = [
   {
-    name: "Entrepreneurship",
-    code: "GSU 08211",
-    credits: 6,
-    teacherId: "12012023045",
+    id: 12012023045,
+    name: "Majogoro",
   },
   {
-    name: "Project",
-    code: "ETU 08221",
-    credits: 12,
-    teacherId: "12012023046",
+    id: 12012023047,
+    name: "J. Ally",
   },
   {
-    name: "Radar",
-    code: "ETU 08222",
-    credits: 9,
-    teacherId: "12012023047",
+    id: 12012023046,
+    name: "Kajange",
   },
   {
-    name: "Broadcasting",
-    code: "ETU 08223",
-    credits: 9,
-    teacherId: "12012023048",
+    id: 12012023048,
+    name: "Nzowa",
   },
   {
-    name: "Satellite",
-    code: "ETU 08224",
-    credits: 9,
-    teacherId: "12012023049",
+    id: 12012023049,
+    name: "Justina",
   },
   {
-    name: "Robotics",
-    code: "COU 08202",
-    credits: 9,
-    teacherId: "12012023050",
-  },
-];
+    id: 12012023050,
+    name: "Dr. Simbeye",
+  }
+]
+
+// const modules = [
+//   {
+//     name: "Entrepreneurship",
+//     code: "GSU 08211",
+//     credits: 6,
+//     teacherId: "12012023045",
+//   },
+//   {
+//     name: "Project",
+//     code: "ETU 08221",
+//     credits: 12,
+//     teacherId: "12012023046",
+//   },
+//   {
+//     name: "Radar",
+//     code: "ETU 08222",
+//     credits: 9,
+//     teacherId: "12012023047",
+//   },
+//   {
+//     name: "Broadcasting",
+//     code: "ETU 08223",
+//     credits: 9,
+//     teacherId: "12012023048",
+//   },
+//   {
+//     name: "Satellite",
+//     code: "ETU 08224",
+//     credits: 9,
+//     teacherId: "12012023049",
+//   },
+//   {
+//     name: "Robotics",
+//     code: "COU 08202",
+//     credits: 9,
+//     teacherId: "12012023050",
+//   },
+// ];
 
 const { Pool } = pkg;
 
@@ -71,20 +98,39 @@ const pool = new Pool({
   connectionTimeoutMillis: 3500,
 });
 
-const insertStudentData = `INSERT INTO STUDENTS (registrationNumber, name) VALUES ($1, $2) RETURNING *`;
+const insertStudentData = `INSERT INTO STUDENTS (registrationNumber, name, class) VALUES ($1, $2, $3) RETURNING *`;
 
 const insertTeacherData = `INSERT INTO TEACHERS (id, name) VALUES ($1, $2)`;
 
 const insertModulesData = `INSERT INTO MODULES (code, name, credits, teacherId) VALUES ($1, $2, $3, $4)`;
 
-async function insertModules() {
+const fetchClasses = `select name, course from class`
+
+// const createAttendanceTable = 
+
+async function createAttendanceTable() {
   try {
     // Loop through the modules array
     for (const _module of modules) {
       const { code, name, credits, teacherId } = _module;
 
+      console.log(`Creating ${name} attendance table` + '\n');
       // Execute the query for each module
-      await pool.query(insertModulesData, [code, name, credits, teacherId]);
+      await pool.query(`
+      -- Create Student Module Attendance Table
+      CREATE TABLE IF NOT EXISTS ${name}(
+        id serial primary key not null,
+        name text,
+        idnumber bigint references students(registrationnumber),
+        role text default 'Student',
+        lesson1 boolean,
+        lesson2 boolean,
+        lesson3 boolean,
+        lesson4 boolean,
+        lesson5 boolean,
+        lesson6 boolean,
+        lesson7 boolean
+      );`);
     }
 
     console.log("Modules inserted successfully!");
@@ -92,22 +138,36 @@ async function insertModules() {
   } catch (err) {
     console.error("Error inserting modules:", err);
     return { success: false };
-  } finally {
-    // Release the client back to the pool
-    pool.end();
+  }
+}
+
+async function insertTeachers() {
+  try {
+    // Loop through the teachers array
+    for (const teacher of teachers) {
+      const { id, name } = teacher;
+      // Execute the query for each teacher
+      await pool.query(insertTeacherData, [id, name]);
+    }
+    console.log("Teachers inserted successfully!");
+    return { success: true };
+  } catch (err) {
+    console.error("Error inserting teachers:", err);
+    return { success: false };
   }
 }
 
 const fetchTeachers = `
   SELECT * FROM teachers
 `;
-const fetchModules = `SELECT * FROM modules`;
+async function fetchModules() {
+  const modules = await pool.query(`SELECT * FROM modules`);
+  return modules.rows;
+};
 
 const fetchStudents = `SELECT * FROM students`;
 
 app.get("/", async (req, res) => {
-  // const result = await insertModules();
-  // console.log(result);
   return res.render("pages/index", {
     title: "homepage",
   });
@@ -119,7 +179,6 @@ app.get("/home", (req, res) => {
 
 app.get("/students", async (req, res) => {
   const students = await pool.query(fetchStudents);
-  console.log(students.rows);
   return res.render("pages/students", {
     title: "students page",
     navlinks: navlinks.students,
@@ -128,21 +187,46 @@ app.get("/students", async (req, res) => {
 });
 
 app.get("/new-student", async (req, res) => {
+  const classes = await pool.query(fetchClasses);
   return res.render("pages/new-student", {
     title: "new student page",
     navlinks: navlinks.students,
+    classes: classes.rows,
   });
 });
 
 app.get("/teachers", async (req, res) => {
-  const teachers = await pool.query(fetchTeachers);
-  console.log(teachers.rows);
+  const currentUrl = req.originalUrl;
+  const teachers = await pool.query(`SELECT * FROM teachers`);
   return res.render("pages/teachers", {
     title: "teachers page",
     navlinks: navlinks.teachers,
     teachers: teachers.rows,
+    currentUrl,
   });
 });
+
+const getTeacherById = async (teacherId) => {
+  const teacher = await pool.query(`SELECT * FROM teachers where id = $1`, [teacherId]);
+  return teacher.rows[0];
+}
+
+const getTeachersModules = async (teacherId) => {
+  const modules = await pool.query(`SELECT * FROM modules where teacherid = $1`, [teacherId]);
+  return modules.rows;
+}
+
+app.get('/teachers/:id', async (req, res) => {
+  let teacherId = req.params.id;
+  const teacher = await getTeacherById(teacherId);
+  const modules = await getTeachersModules(teacherId);
+  return res.render("pages/teacher-details", {
+    title: "Teacher details page",
+    navlinks: navlinks.teachers,
+    teacher,
+    modules,
+  });
+})
 
 app.get("/new-teacher", async (req, res) => {
   return res.render("pages/new-teacher", {
@@ -152,10 +236,12 @@ app.get("/new-teacher", async (req, res) => {
 });
 
 app.get("/classes", async (req, res) => {
+  const classes = await pool.query(fetchClasses);
   return res.render("pages/classes", {
     title: "Classes page",
     currentUrl: req.originalUrl,
     navlinks: navlinks.classes,
+    classes: classes.rows,
   });
 });
 
@@ -169,6 +255,8 @@ app.get("/classes/:name", async (req, res) => {
 });
 
 app.get("/modules", async (req, res) => {
+  const modules = await fetchModules();
+  console.log(modules);
   return res.render("pages/modules", {
     title: "Modules page",
     currentUrl: req.originalUrl,
@@ -187,11 +275,12 @@ app.get("/modules/:code", async (req, res) => {
 });
 
 app.post("/student-registration", async (req, res) => {
-  const { registrationNumber, name } = req.body;
+  const { registrationNumber, name, _class } = req.body;
   try {
     const student = await pool.query(insertStudentData, [
       registrationNumber,
       name,
+      _class,
     ]);
     console.log("Student inserted successfully!\n", student.rows);
     return res.redirect("/students");
